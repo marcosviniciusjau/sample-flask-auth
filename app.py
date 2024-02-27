d1b6a2c9acddb4556b23965f7a869eb0c3d05c7f
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-02-27 13:29:38 -0300

    Finalization of the User's crud

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request,jsonify
-from flask_login import LoginManager, login_user,current_user
+from flask_login import LoginManager, login_user,current_user,logout_user,login_required
 from models.user import User
 from database import db
 
@@ -33,9 +33,65 @@ def login():
     
     return jsonify({"message": "Credencias inválidas"}),400
 
-@app.route('/hello-world', methods=["GET"])
-def hello_world():
-    return "Hello World"
+@app.route('/logout', methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso"})
+
+
+@app.route('/user', methods=["POST"])
+def create_user():
+    data= request.json
+    username= data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user= User(username= username, password= password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuário cadastrado com sucesso"})
+    
+    return jsonify({"message": "Dados inválidos"}),400
+
+
+@app.route('/user/<int:id_user>', methods=["GET"])
+@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
+
+    if user:
+        return {"username": user.username}
+    return jsonify({"message": "Usuário não encontrado"}),404
+
+@app.route('/user/<int:id_user>', methods=["PUT"])
+@login_required
+def update_user(id_user):
+    data = request.json
+    user = User.query.get(id_user)
+
+    if user and data.get("password"):
+        user.password = data.get("password")
+        db.session.commit()
+
+        return jsonify({"message": f"Usuário {id_user} atualizado com sucesso"})
+    
+    return jsonify({"message": "Usuário não encontrado"}),404
+
+@app.route('/user/<int:id_user>', methods=["DELETE"])
+@login_required
+def delete_user(id_user):
+    user = User.query.get(id_user)
+
+    if id_user == current_user.id:
+        return jsonify({"message": "Voce não pode excluir o usuário atual"}),403
+
+    if user and id_user != current_user.id:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message": f"Usuário {id_user} excluído com sucesso"})
+    
+    retun jsonify({"message": "Usuário não encontrado"}),404
 
 if __name__ == '__main__':
     app.run(debug=True)
